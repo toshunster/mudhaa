31af4f99748d8ba15f7b423c9a8d6a1711157ac8
toshunster <a.kukhtichev@mail.ru>
2017-09-12 15:06:48 +0300

    Database url for heroku.

--- mudhaa/settings.py
+++ mudhaa/settings.py
@@ -78,6 +78,7 @@ WSGI_APPLICATION = 'mudhaa.wsgi.application'
 # Update database configuration with $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config()
+DATABASES = dict()
 DATABASES['default'].update(db_from_env)
 
 #DATABASES = {  
